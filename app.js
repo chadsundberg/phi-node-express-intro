03dbbea899aa88757c79cb5ec8731adb4c847afe
Chad Sundberg <chadsundberg@gmail.com>
2017-02-22 08:30:40 -0600

    notes

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('public')); // this is saying, if it's looking for a static file, which this is... it says look in the folder public
+// app.use runs every time.  This line handles the 'get' request.  All of our 'static' files, index.html, & client.js are accessed via 'get' requests.
+// we need a way to get data back from the server
+app.use(bodyParser.urlencoded({extended: true}));
+
+var songList = [
+  {
+    title: 'We did not start the Phire',
+    artist: 'Billy Joel'
+  },
+  {
+    title: 'Ring of Phire',
+    artist: 'Johnny Cash'
+  }
+];
+
+//the next bit in our 'get' request, contains a function that takes in a req and res, request and response, from a server
+app.get('/songs', function(req, res) {  // this is a get request for songs
+  res.send(songList); // has to match 'GET' in client.js
+// '/songs' also has to match in client.js
+});
+
+app.post('/newSong', function(req, res) {
+  var newSong = req.body;
+  if(newSong.artist !== "Justin Bieber") {
+   // data from client.js, becomes req.body
+  songList.push(newSong);
+  console.log(songList);
+  res.sendStatus(200);
+} else {
+  res.sendStatus(500);
+}
+});
+
+app.listen(3000);
+// app.listen only runs one time at the beginning, and keeps listening
--- public/client.js
+++ public/client.js
@@ -1,3 +1,40 @@
 $(document).ready(function() {
   console.log('jquery was sourced!');
+  getAllSongs();
+  function getAllSongs() {
+  $.ajax({  // this code gives us a way to get our song array and display it
+    type: 'GET', // it's a 'GET' request to ask for information from somewhere
+    url: '/songs',  // this goes back and matches our /songs in our app.js file
+    success: function(response) {  // on success triggers a function that takes in a response and displays the information on the console log.
+      console.log('response', response); // res.send(songList) in app.js sends the songList
+    },
+    error: function(response) {
+      console.log('error', error);
+    }
+  });
+}
+
+$('#addSongButton').on('click', function(){  //this code allows us to add songs to songList, which is on the server.  It allows us to add information into that array from the DOM!
+  // console.log('here');
+  var newSongTitle = $('#title').val(); // these ids allow us to get info from the DOM
+  var newSongArtist = $('#artist').val();
+  var newSongObject = { // builds out a new song object
+    title: newSongTitle,
+    artist: newSongArtist,
+  };
+  console.log(newSongObject);
+  $.ajax({ // this is the request that allows us to add the newly entered song into the array
+    type: 'POST',
+    url: '/newSong',
+    data: newSongObject,
+    success: function(response) {
+      console.log('response', response);
+      getAllSongs();
+    },
+    error: function(response) {
+      console.log('error', error);
+    }
+  });
+});
+
 });
